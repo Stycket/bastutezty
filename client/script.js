fd4d66b8a02abf2037524a210783d867e5c290bc
Stycket <121405173+Stycket@users.noreply.github.com>
2023-08-24 17:51:25 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,4 +1,5 @@
-import { DateTime } from 'luxon';
+ 
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -25,6 +26,8 @@ document.addEventListener('click', (event) => {
 
   const selectableDate = target.closest('.selectable-date');
   const selectableTime = target.closest('.selectable-time');
+
+ 
 
   if (selectableDate) {
     handleDateSelection(selectableDate);
@@ -83,13 +86,7 @@ function populateTimeSlots(selectedDate, selectedMonth) {
     const cellId = `Date_${selectedDateElement.textContent}-Time_${formattedHour}`;
   
 
-
-
-
-
     bodyCell.id = cellId.replace(/\s+/g, '-'); // Replace spaces with dashes
-
-
 
 
     const bookedItemsList = document.getElementById("booked_items_list");
@@ -107,7 +104,7 @@ function populateTimeSlots(selectedDate, selectedMonth) {
               const tableCell = document.getElementById(tableCellId.slice(0, -3));
   
               if (tableCell) {
-                  tableCell.style.backgroundColor = "red";
+                  tableCell.style.backgroundColor = "blueviolet";
                   tableCell.style.pointerEvents = "none";
               }
           }
@@ -134,7 +131,7 @@ function populateTimeSlots(selectedDate, selectedMonth) {
            
 
             if (tableCell) {
-                tableCell.style.backgroundColor = "red";
+                tableCell.style.backgroundColor = "blueviolet";
                 tableCell.style.pointerEvents = "none";
             }
         }
@@ -168,11 +165,14 @@ function populateTimeSlots(selectedDate, selectedMonth) {
 
   timeSlotTable.appendChild(timeSlotTableBody);
 
+
+  const bookingBox = document.getElementById('bookin_box');
+
   const existingTable = document.getElementById('time-slot-table');
   if (existingTable) {
     existingTable.replaceWith(timeSlotTable);
   } else {
-    document.body.appendChild(timeSlotTable);
+    bookingBox.appendChild(timeSlotTable);
   }
 }
 
@@ -205,6 +205,35 @@ function handleTimeSelection(selectedTimeElement) {
 
   console.log('Selected times:', selectedTimes.map(time => time.textContent));
 
+  const choosenTimesDiv = document.getElementById('BookingTimes');
+  const choosenTimes = Array.from(selectedTimes).map(time => time.textContent);
+
+  // Convert the time strings to Date objects for easier manipulation
+  const timeObjects = choosenTimes.map(time => {
+    const [hours, minutes] = time.split(':');
+    return new Date(0, 0, 0, parseInt(hours), parseInt(minutes));
+  });
+  
+  // Find the earliest and latest times
+  const earliestTime = new Date(Math.min(...timeObjects));
+  const latestTime = new Date(Math.max(...timeObjects));
+  
+  // Add 1 hour to the latest time
+  latestTime.setHours(latestTime.getHours() + 1);
+  
+  // Format the times as strings in the desired format (HH:mm-HH:mm)
+  const formattedEarliestTime = `${earliestTime.getHours()}:${earliestTime.getMinutes().toString().padStart(2, '0')}`;
+  const formattedLatestTime = `${latestTime.getHours()}:${latestTime.getMinutes().toString().padStart(2, '0')}`;
+  
+  // Combine the formatted times with a hyphen
+  const formattedTimeRange = `${formattedEarliestTime}-${formattedLatestTime}`;
+  
+  // Assign the formatted time range to the chosenTimesDiv's text content
+  choosenTimesDiv.textContent = formattedTimeRange;
+  
+
+
+  
   // Log the cell ID with the correct values
   const cellId = `Date_${selectedDate.textContent}-Time_${selectedTimeElement.textContent}`;
 
@@ -240,15 +269,31 @@ function handleTimeSelection(selectedTimeElement) {
       button.classList.add('month-button');
       button.dataset.month = monthIndex + 1; // Adding 1 to match 1-indexed months
       monthButtonsContainer.appendChild(button);
+
+
+  // Add the 'selected' class to the first button
+  if (i === -1) {
+    button.classList.add('selected');
+}
+
     
       button.addEventListener('click', () => {
+
+
+  // Remove selected class from all buttons
+      const allButtons = document.querySelectorAll('.month-button');
+      allButtons.forEach(btn => btn.classList.remove('selected'));
+
+      // Add selected class to the clicked button
+      button.classList.add('selected');
+
+
+
         selectedMonth = monthIndex + 1; // Adding 1 to match 1-indexed months
         populateDateSelector(selectedMonth);
       });
     }
   }
-
-
 
 
 
@@ -263,13 +308,39 @@ function handleTimeSelection(selectedTimeElement) {
 
   let row = document.createElement('tr');
 
+  let rowDays = document.createElement('tr');
+
+
+
+const firstDayOfWeek = startDate.weekday; // Get the day index (0-6) of the 1st day
+  const daysOfWeek = ['Sön', 'Mån', 'Tis', 'Ons', 'Tors', 'Fre', 'Lör'];
+
+  let headerRow = document.createElement('tr');
+  for (let i = 0; i < 7; i++) {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = daysOfWeek[(firstDayOfWeek + i) % 7]; // Adjust for the first day of the week
+    headerRow.appendChild(headerCell);
+  }
+  dateTable.appendChild(headerRow);
+
+  row.classList.add('week-row');
+
+  for (let i = 0; i < firstDayOfWeek; i++) {
+    const emptyCell = document.createElement('td');
+    emptyCell.classList.add('empty-cell');
+    rowDays.appendChild(emptyCell);
+  }
+
+
+
   while (startDate <= endDate) {
     const cell = document.createElement('td');
     const option = document.createElement('option');
     option.value = startDate.toISODate();
 
     // Format the date in Swedish
-    const formattedDate = startDate.setLocale('sv').toFormat('cccc LLLL d');
+    const formattedDate = startDate.setLocale('sv').toFormat('d / M');
+
     option.textContent = formattedDate;
 
     cell.appendChild(option);
@@ -282,17 +353,21 @@ function handleTimeSelection(selectedTimeElement) {
     }
 
     startDate = startDate.plus({ days: 1 });
+    
   }
 
   if (row.children.length > 0) {
     dateTable.appendChild(row);
   }
 
+
+  const bookingBox = document.getElementById('bookin_box');
+
   const existingTable = document.getElementById('date-table');
   if (existingTable) {
     existingTable.replaceWith(dateTable);
   } else {
-    document.body.appendChild(dateTable);
+    bookingBox.appendChild(dateTable);
   }
 
   // Re-add event listeners for the new date cells
@@ -303,12 +378,6 @@ function handleTimeSelection(selectedTimeElement) {
     });
   });
 }
-
-
-
-
-
-
 
 
 
@@ -324,11 +393,14 @@ function handleTimeSelection(selectedTimeElement) {
     const selectedDateValue = selectedDate.textContent;
     const selectedTimeSlots = selectedTimes.map(time => time.textContent);
 
+    //reload page when bookin
+    location.reload();
+
 
     console.log('Selected date:', selectedDateValue);
   console.log('Selected time slots:', selectedTimeSlots);
   
-    const response = await fetch('http://localhost:3000/api/book', {
+    const response = await fetch('https://6219-2a00-801-703-2b1b-f0f1-441b-b075-5c37.ngrok-free.app/api/book', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -358,7 +430,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   const messageBox = document.querySelector('#messageBoxId');
 
   try {
-    const response = await fetch('http://localhost:3000/api/booked');
+    const response = await fetch('https://6219-2a00-801-703-2b1b-f0f1-441b-b075-5c37.ngrok-free.app/api/booked');
     const data = await response.json();
 
     if (data.success) {
@@ -373,16 +445,69 @@ window.addEventListener('DOMContentLoaded', async () => {
 const bookedItemsListDiv = document.createElement('div');
 bookedItemsListDiv.id = 'booked_items_list';
 
+// booked slots logic for user
+
+const bokadeTider = document.querySelector('#bokadeTider'); // Replace with your list element's ID
+
 // Loop through booked items and create list items
 bookedItems.forEach(item => {
   const listItem = document.createElement('li');
+  const listItemUser = document.createElement('li');
   const text = `Date_${item.date}-Time_${item.timeSlot.map(slot => slot.split(':')[0]).join('-')}_id`;
+
+  const textUser = `Datum: ${item.date} Tid: ${item.timeSlot.map(slot => slot.split(':')[0]).join('-')}`;
 
   
   listItem.textContent = text;
   listItem.id = text.replace(/\s+/g, '-'); // Replace spaces with dashes
+
+
+
+
+
+
+  function reformatDateTime(input) {
+    const match = input.match(/Tid: (\d+)(?:-(\d+))?(?:-(\d+))?/);
+  
+    if (!match) {
+      return input; // If no match is found, return the original string
+    }
+  
+    const [, first, middle, last] = match.map(Number);
+  
+    let startTime, endTime;
+  
+    if (last) {
+      startTime = Math.min(first, middle, last);
+      endTime = Math.max(first, middle, last) + 1;
+    } else if (middle) {
+      startTime = first;
+      endTime = middle + 1;
+    } else {
+      startTime = first;
+      endTime = first + 1;
+    }
+  
+    const formattedStartTime = `${startTime < 10 ? '0' : ''}${startTime}:00`;
+    const formattedEndTime = `${endTime < 10 ? '0' : ''}${endTime}:00`;
+  
+    return input.replace(/Tid: \d+(?:-\d+)?(?:-\d+)?/, `Tid: ${formattedStartTime}-${formattedEndTime}`);
+  }
+  
+  const inputString = textUser;
+  const reformattedString = reformatDateTime(inputString);
+  
+  console.log(reformattedString);
+
+
+
+
+  
+  listItemUser.textContent = reformattedString;
   
   bookedItemsListDiv.appendChild(listItem);
+
+  bokadeTider.appendChild(listItemUser);
 });
 
 // Append the div containing the list items to the parent element
